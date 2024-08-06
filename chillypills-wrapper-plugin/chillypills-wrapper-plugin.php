3cba1a7d808270508710934c05e909f8826abbca
Alvaro Puche Ortiz <alvaropuche0001@gmail.com>
2024-08-06 08:59:15 +0200

    Update plugin versions and improve installation process

--- chillypills-wrapper-plugin/chillypills-wrapper-plugin.php
+++ chillypills-wrapper-plugin/chillypills-wrapper-plugin.php
@@ -2,7 +2,7 @@
 /*
 Plugin Name: Chillypills Wrapper Plugin
 Description: Plugin principal para gestionar las licencias de otros plugins Chillypills y gestionar la instalación y activación de plugins.
-Version: 1.1.0
+Version: 0.0.2
 Author: Álvaro Puche Ortiz x Chillypills Comunicación S.L.
 Author URI: https://chillypills.com
 */
@@ -19,7 +19,7 @@ class Chillypills_Wrapper_Plugin {
         add_action('admin_menu', array($this, 'create_menu'));
         add_action('admin_init', array($this, 'register_settings'));
         add_action('admin_enqueue_scripts', array($this, 'enqueue_styles'));
-        add_action('admin_post_chillypills_download_plugin', array($this, 'handle_download_plugin'));
+        add_action('pre_set_site_transient_update_plugins', array($this, 'check_for_plugin_updates'));
     }
 
     public function create_menu() {
@@ -93,6 +93,7 @@ class Chillypills_Wrapper_Plugin {
         }
 
         $chillypills_plugins = $plugins_data['plugins'];
+        $installed_plugins = get_plugins();
 
         ?>
         <div class="wrap">
@@ -108,17 +109,29 @@ class Chillypills_Wrapper_Plugin {
                 <tbody>
                     <?php foreach ($chillypills_plugins as $plugin_slug => $plugin_data): ?>
                         <?php
-                        $plugin_name = isset($plugin_data['name']) ? $plugin_data['name'] : $plugin_slug;
-                        $plugin_description = isset($plugin_data['description']) ? $plugin_data['description'] : '';
-                        $download_url = isset($plugin_data['download_url']) ? $plugin_data['download_url'] : '#';
+                        $plugin_file = $plugin_slug . '/' . $plugin_slug . '.php';
+                        $is_installed = isset($installed_plugins[$plugin_file]);
+                        $is_active = $is_installed && is_plugin_active($plugin_file);
+                        $plugin_version = isset($plugin_data['current_version']) ? $plugin_data['current_version'] : 'Desconocida';
                         ?>
                         <tr>
                             <td class="plugin-title">
-                                <strong><?php echo esc_html($plugin_name); ?></strong>
+                                <strong><?php echo esc_html($plugin_data['name']); ?></strong>
                             </td>
-                            <td class="column-description"><?php echo esc_html($plugin_description); ?></td>
+                            <td class="column-description"><?php echo esc_html($plugin_data['description']); ?></td>
                             <td class="column-actions">
-                                <a href="<?php echo esc_url($download_url); ?>" class="button button-primary" download>Descargar</a>
+                                <?php if ($is_installed): ?>
+                                    <?php if ($is_active): ?>
+                                        <span class="button button-disabled">Activo</span>
+                                    <?php else: ?>
+                                        <a href="<?php echo esc_url(wp_nonce_url('plugins.php?action=activate&plugin=' . $plugin_file, 'activate-plugin_' . $plugin_file)); ?>" class="button button-primary">Activar</a>
+                                    <?php endif; ?>
+                                <?php else: ?>
+                                    <a href="<?php echo esc_url($plugin_data['download_url']); ?>" class="button button-primary" download>Descargar e Instalar</a>
+                                <?php endif; ?>
+                                <?php if ($is_installed && version_compare($installed_plugins[$plugin_file]['Version'], $plugin_version, '<')): ?>
+                                    <a href="<?php echo esc_url($plugin_data['download_url']); ?>" class="button button-secondary" download>Actualizar</a>
+                                <?php endif; ?>
                             </td>
                         </tr>
                     <?php endforeach; ?>
@@ -128,8 +141,35 @@ class Chillypills_Wrapper_Plugin {
         <?php
     }
 
-    public function handle_download_plugin() {
-        // No es necesario para esta funcionalidad simplificada.
+    public function check_for_plugin_updates($transient) {
+        if (empty($transient->checked)) {
+            return $transient;
+        }
+
+        $plugins_json_url = 'https://plugins-control.chillypills.com/plugins.json';
+        $response = wp_remote_get($plugins_json_url);
+
+        if (is_wp_error($response)) {
+            return $transient;
+        }
+
+        $plugins_data = json_decode(wp_remote_retrieve_body($response), true);
+        if (!isset($plugins_data['plugins'])) {
+            return $transient;
+        }
+
+        foreach ($plugins_data['plugins'] as $plugin_slug => $plugin_info) {
+            $plugin_file = $plugin_slug . '/' . $plugin_slug . '.php';
+            if (isset($transient->checked[$plugin_file]) && version_compare($plugin_info['current_version'], $transient->checked[$plugin_file], '>')) {
+                $transient->response[$plugin_file] = (object) [
+                    'slug' => $plugin_file,
+                    'new_version' => $plugin_info['current_version'],
+                    'package' => $plugin_info['download_url'],
+                ];
+            }
+        }
+
+        return $transient;
     }
 
     public function enqueue_styles() {
